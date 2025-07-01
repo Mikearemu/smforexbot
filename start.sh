70891bbacaf9a31c17d98e3193484778a9ab4c7f
Stackmike <mikearemu05@gmail.com>
2025-07-01 14:32:31 +0100

    Added Build SH

--- start.sh
+++ start.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
+# Install TA-Lib C Library
+apt-get update && apt-get install -y build-essential wget
+wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
+tar -xvzf ta-lib-0.4.0-src.tar.gz
+cd ta-lib
+./configure --prefix=/usr
+make
+make install
+cd ..
+
+# Install Python packages
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Run the bot
 python forex_signal_bot.py
